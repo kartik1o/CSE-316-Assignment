4c7cb1f3632d1e9af5909020124c0e16a56ce662
kartik1o <49616171+kartik1o@users.noreply.github.com>
2019-04-17 14:07:30 +0530

    update required

---
+++ OS.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int PRate,Memor,Page_Fa,Memoe,Pager,Time_To_Modify,Pagr,Tce;
+    float Ef,Pag=100/16.4e6,Tiy=8/1e3;
+	float Tim=20/1e3,Me=100/1e9;Pagr=.7;
+    Ef=(1-PRate)*(Memor)+(Page_Fa)*(Memoe + (1-Pager)*(Time_To_Modify)+(Pagr)*(Tce));
+    printf("%f",Ef);
+    return 0;
+}
+
